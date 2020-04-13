9560a31933469cb0f0da6e83a3ff12309b1150bb
jonheikkinen <jheikk2@uic.edu>
2020-04-13 00:33:44 -0500

    Finally got messages working. Game is able to play multiple rounds now and outputs all relevant info to clients and server after each step. Still need to figure out how to stop game and allow them to choose to play another.

--- src/main/java/MorraInfo.java
+++ src/main/java/MorraInfo.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 
 public class MorraInfo implements Serializable
 {
-    int p1Points, p2Points,p1Plays, p2Plays,p1Guess,p2Guess;;
+    int p1Points, p2Points,p1Plays, p2Plays,p1Guess,p2Guess;
     int playerNumber;
     Boolean have2Players;
 
@@ -10,25 +10,29 @@ public class MorraInfo implements Serializable
     {
         p1Points = 0;
         p2Points = 0;
+        p1Plays = 0;
+        p2Plays = 0;
+        p1Guess = 0;
+        p2Guess = 0;
 
         have2Players = false;
     }
 
     int evalPlay()
     {
-            int total = p1Plays + p2Plays;
+        int total = p1Plays + p2Plays;
 
-            //check to see if both guessed right, award no points
-            if(p1Guess == total && p2Guess == total)
-                return 0;
-            //check if p1 guessed right, award 1 point
-            if (p1Guess == total)
-                return 1;
-                //check if p2 guessed right, award 1 point
-            else if (p2Guess == total)
-                return 2;
-                //if we get here, neither guessed right, award no points
-            else
-                return 0;
+        //check to see if both guessed right, award no points
+        if(p1Guess == total && p2Guess == total)
+            return 0;
+        //check if p1 guessed right, award 1 point
+        if (p1Guess == total)
+            return 1;
+            //check if p2 guessed right, award 1 point
+        else if (p2Guess == total)
+            return 2;
+            //if we get here, neither guessed right, award no points
+        else
+            return 3;
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -61,12 +61,21 @@ public class Server{
                             //update server MorraInfo with player number, then send that to client
                             //This allows each client to know which player number they are
                             gameInfo.playerNumber = i + 1;
+
+                            //create temp object that is equal to each client thread
                             ClientThread t = clients.get(i);
                             try {
+                                //writes out servers MorraInfo class to each respective client
+                                //each one contains a different player number
+                                t.out.writeObject("You are Player #" + gameInfo.playerNumber);
+                                t.out.reset();
+                                t.out.flush();
                                 t.out.writeObject(gameInfo);
                                 t.out.reset();
+                                t.out.flush();
                             }
                             catch(Exception e) {}
+                            //print check to make sure each client gets appropriate player number
                             System.out.println("Client #" + i + " is player #" + clients.get(i).playerNumber);
                         }
                     }
@@ -93,12 +102,17 @@ public class Server{
             this.count = count;
         }
 
-        public void updateClients(MorraInfo gameInfo) {
+        public void updateClients(String message) {
             for(int i = 0; i < clients.size(); i++) {
                 ClientThread t = clients.get(i);
                 try {
+                    t.out.writeObject(message);
+                    t.out.reset();
+                    t.out.flush();
+                    gameInfo.playerNumber = i+1;
                     t.out.writeObject(gameInfo);
                     t.out.reset();
+                    t.out.flush();
                 }
                 catch(Exception e) {}
             }
@@ -123,49 +137,129 @@ public class Server{
                     //only one played has joined the server
                     if(stageCounter == 0)
                     {
-                        //updateClients("Awaiting another player");
+                        updateClients("Awaiting another player");
                         stageCounter++;//go to next stage
                     }
                     //Wait for Second player to join the server
                     if(stageCounter == 1)
                     {
                         if (gameInfo.have2Players) {
-//                            updateClients("Two players have joined");
-//                            updateClients("Welcome to the ancient game of Morra!");
-//                            updateClients("Choose a number 0-5");
+                            updateClients("Two players have joined");
+                            updateClients("Welcome to the ancient game of Morra!");
+                            updateClients("Choose a number 0-5");
+                            updateClients("Then guess the combined total of you and your opponents numbers");
                             stageCounter++;//go to next stage
                         }
                     }
                     //Game has begun
-                    String data = in.readObject().toString();
+                    MorraInfo data = (MorraInfo) in.readObject();
                     if(stageCounter == 2) {
-                        //FIXME this block of code doesn't work correctly
-                        //only recognizes player 2 sending info, uncomment and try it
-                        callback.accept("client #" + count + " sent: " + data);
-                        //updateClients("client #" + count + " said: " + data);
-                        inputCounter++;
 
-                        if(playerNumber == 1)
-                        {
-                            System.out.println("Player 1 sent info");
+                        if(data.playerNumber == 1) {
+                            gameInfo.p1Plays = data.p1Plays;
+                            gameInfo.p1Guess = data.p1Guess;
+                            callback.accept("Player #" + data.playerNumber + " played " + gameInfo.p1Plays);
+                            callback.accept("Player #" + data.playerNumber + " guessed " + gameInfo.p1Guess);
+                            inputCounter++;
+                            //FIXME add this when messages are working
+                            //if(inputCounter < 2)
+                                //send message to client that server is waiting for other player
                         }
-                        if(playerNumber == 2)
-                            System.out.println("Player 2 sent info");
+                        if(data.playerNumber == 2) {
+                            gameInfo.p2Plays = data.p2Plays;
+                            gameInfo.p2Guess = data.p2Guess;
+                            callback.accept("Player #" + data.playerNumber + " played " + gameInfo.p2Plays);
+                            callback.accept("Player #" + data.playerNumber + " guessed " + gameInfo.p2Guess);
+                            inputCounter++;
+                            //FIXME add this when messages are working
+                            //if(inputCounter < 2)
+                                //send message to client that server is waiting for other player
+                        }
 
-                        System.out.println("StageCounter = " + stageCounter);
-                        if(inputCounter == 4)
+                        if(inputCounter == 2)
                         {
                             callback.accept("Both players have their input");
-                            callback.accept("Moving to next stage of game");
+                            callback.accept("Moving to play stage of the game");
                             stageCounter++;
-                            System.out.println("Stage counter = " + stageCounter);
                         }
 
                     }
                     //evaluate which player was awarded points, if any
                     if(stageCounter == 3)
                     {
+                        int result = gameInfo.evalPlay();
+                        if(result == 0) {
+                            updateClients("Both players guessed correct, no points awarded");
+                            callback.accept("Both players guessed correct, no points awarded");
+                        }
+                        if(result == 1)
+                        {
+                            updateClients("Player 1 is correct, player 1 gets 1 point");
+                            callback.accept("Player 1 is correct, gets 1 point!");
+                            gameInfo.p1Points++;
+                        }
+                        if(result == 2)
+                        {
+                            updateClients("Player 2 is correct, player 2 gets 1 point");
+                            callback.accept("Player 2 is correct, gets 1 point!");
+                            gameInfo.p2Points++;
+                        }
+                        if(result == 3) {
+                            updateClients("Neither player is correct, no points awarded");
+                            callback.accept("Neither player is correct, no points awarded");
+                        }
 
+                        //tell player 1 what player 2 played and guessed
+                        ClientThread t = clients.get(0);
+                        try {
+                            t.out.writeObject("Player 2 played " + gameInfo.p2Plays + " and guessed " + gameInfo.p2Guess);
+                            t.out.reset();
+                            t.out.flush();
+                            gameInfo.playerNumber = 1;
+                            t.out.writeObject(gameInfo);
+                            t.out.reset();
+                            t.out.flush();
+                        }
+                        catch(Exception e) {}
+
+                        //tell player 2 what player 1 played and guessed
+                        t = clients.get(1);
+                        try {
+                            t.out.writeObject("Player 1 played " + gameInfo.p1Plays + " and guessed " + gameInfo.p1Guess);
+                            t.out.reset();
+                            t.out.flush();
+                            gameInfo.playerNumber = 2;
+                            t.out.writeObject(gameInfo);
+                            t.out.reset();
+                            t.out.flush();
+                        }
+                        catch(Exception e) {}
+
+                        //Display points to server and clients
+                        callback.accept("Player 1 has " + gameInfo.p1Points + " points");
+                        callback.accept("Player 2 has " + gameInfo.p1Points + " points");
+                        callback.accept("Beginning next round");
+                        callback.accept("-------------------------");
+                        updateClients("Player 1 Points: " + gameInfo.p1Points);
+                        updateClients("Player 2 Points: " + gameInfo.p2Points);
+
+                        //display if player 1 has won
+                        if(gameInfo.p1Points == 2)
+                        {
+                            updateClients("!!!!!!!! Player 1 has won with 2 points!");
+                        }
+                        //display if player 2 has won
+                        else if(gameInfo.p2Points == 2)
+                        {
+                            updateClients("!!!!!!!! Player 2 has won with 2 points!");
+                        }
+                        //otherwise reset round by going back to 2nd stage and setting input count to 0
+                        else {
+                            updateClients("Beginning next round");
+                            updateClients("-------------------------------");
+                            stageCounter = 2;
+                            inputCounter = 0;
+                        }
                     }
 
                 }
@@ -173,6 +267,8 @@ public class Server{
                     callback.accept("OOOOPPs...Something wrong with the socket from client: " + count + "....closing down!");
                     //updateClients("Client #" + count + " has left the server!");
                     clients.remove(this);
+                    updateClients("Awaiting another player");
+                    stageCounter = 1;
                     break;
                 }
             }
